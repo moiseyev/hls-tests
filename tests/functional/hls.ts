6c270aa1d546dc45df4257c884a7bd6ee36ab33d
Alexander Moiseyev <alexander@moiseyev.name>
2017-09-28 16:14:07 +0300

    Add hls test

---
+++ tests/functional/hls.ts
@@ -0,0 +1,24 @@
+import { } from 'intern';
+
+const { suite, test, before } = intern.getInterface('tdd');
+const { assert } = intern.getPlugin('chai');
+
+suite('hls', () => {
+    before(async ({ remote }) => {
+        await remote.get('tests/functional/hls.html');
+        await remote.sleep(1000);
+    });
+
+    test('canPlayType application/vnd.apple.mpegURL', async ({ remote }) => {
+        assert.equal(await remote.findById('can-play-type-1').getVisibleText(), 'maybe');
+    });
+
+    test('canPlayType application/x-mpegurl', async ({ remote }) => {
+        assert.equal(await remote.findById('can-play-type-2').getVisibleText(), 'maybe');
+    });
+
+    test('play', async ({ remote }) => {
+        await remote.findById('play-button').click();
+        assert.equal(await remote.findById('video').getProperty('paused'), false);
+    });
+});
